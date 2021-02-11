5fe1dfb7e8cdbbc5e9ad1827861c4f0cdec4c3db
IronFist <55384202+PurnanshDavawala@users.noreply.github.com>
2021-02-12 10:15:54 +1300

    Update PowerView_dev.ps1

--- PowerView_dev.ps1
+++ PowerView_dev.ps1
@@ -1,31 +1,8 @@
-#requires -version 2
 
 
 
 #################################################
 function New-InMemoryModule {
-<#
-.SYNOPSIS
-
-Creates an in-memory assembly and module
-
-
-.DESCRIPTION
-
-When defining custom enums, structs, and unmanaged functions, it is
-necessary to associate to an assembly module. This helper function
-creates an in-memory module that can be passed to the 'enum',
-'struct', and Add-Win32Type functions.
-
-.PARAMETER ModuleName
-
-Specifies the desired name for the in-memory assembly and module. If
-ModuleName is not provided, it will default to a GUID.
-
-.EXAMPLE
-
-$Module = New-InMemoryModule -ModuleName Win32
-#>
 
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
     [CmdletBinding()]
@@ -107,98 +84,7 @@ function func {
 
 function Add-Win32Type
 {
-<#
-.SYNOPSIS
 
-Creates a .NET type for an unmanaged Win32 function.
-
-Author: Matthew Graeber (@mattifestation)
-License: BSD 3-Clause
-Required Dependencies: None
-Optional Dependencies: func
-
-.DESCRIPTION
-
-Add-Win32Type enables you to easily interact with unmanaged (i.e.
-Win32 unmanaged) functions in PowerShell. After providing
-Add-Win32Type with a function signature, a .NET type is created
-using reflection (i.e. csc.exe is never called like with Add-Type).
-
-The 'func' helper function can be used to reduce typing when defining
-multiple function definitions.
-
-.PARAMETER DllName
-
-The name of the DLL.
-
-.PARAMETER FunctionName
-
-The name of the target function.
-
-.PARAMETER EntryPoint
-
-The DLL export function name. This argument should be specified if the
-specified function name is different than the name of the exported
-function.
-
-.PARAMETER ReturnType
-
-The return type of the function.
-
-.PARAMETER ParameterTypes
-
-The function parameters.
-
-.PARAMETER NativeCallingConvention
-
-Specifies the native calling convention of the function. Defaults to
-stdcall.
-
-.PARAMETER Charset
-
-If you need to explicitly call an 'A' or 'W' Win32 function, you can
-specify the character set.
-
-.PARAMETER SetLastError
-
-Indicates whether the callee calls the SetLastError Win32 API
-function before returning from the attributed method.
-
-.PARAMETER Module
-
-The in-memory module that will host the functions. Use
-New-InMemoryModule to define an in-memory module.
-
-.PARAMETER Namespace
-
-An optional namespace to prepend to the type. Add-Win32Type defaults
-to a namespace consisting only of the name of the DLL.
-
-.EXAMPLE
-
-$Mod = New-InMemoryModule -ModuleName Win32
-
-$FunctionDefinitions = @(
-  (func kernel32 GetProcAddress ([IntPtr]) @([IntPtr], [String]) -Charset Ansi -SetLastError),
-  (func kernel32 GetModuleHandle ([Intptr]) @([String]) -SetLastError),
-  (func ntdll RtlGetCurrentPeb ([IntPtr]) @())
-)
-
-$Types = $FunctionDefinitions | Add-Win32Type -Module $Mod -Namespace 'Win32'
-$Kernel32 = $Types['kernel32']
-$Ntdll = $Types['ntdll']
-$Ntdll::RtlGetCurrentPeb()
-$ntdllbase = $Kernel32::GetModuleHandle('ntdll')
-$Kernel32::GetProcAddress($ntdllbase, 'RtlGetCurrentPeb')
-
-.NOTES
-
-Inspired by Lee Holmes' Invoke-WindowsApi http://poshcode.org/2189
-
-When defining multiple function prototypes, it is ideal to provide
-Add-Win32Type with an array of function signatures. That way, they
-are all incorporated into the same in-memory module.
-#>
 
     [OutputType([Hashtable])]
     Param(
